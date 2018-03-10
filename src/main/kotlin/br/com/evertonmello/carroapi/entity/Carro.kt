ab550002f8314b37c5c5a41e2f25841517d10b82
evertonmello <evertonmello@live.com>
2018-03-10 13:58:33 -0300

    url img

--- src/main/kotlin/br/com/evertonmello/carroapi/entity/Carro.kt
+++ src/main/kotlin/br/com/evertonmello/carroapi/entity/Carro.kt
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
-data class Carro(@Id var id: String, var marca: String, var modelo: String, var ano: Int){
+data class Carro(@Id var id: String, var marca: String, var modelo: String, var ano: Int, var urlImagem: String?){
 
     /*constructor():this("", "", "", 0 )*/
 
